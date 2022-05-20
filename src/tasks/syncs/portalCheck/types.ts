b78abc8bdaa388ec9377e76b7cd2750fc5d3cec4
Lorenz Ras <lenzras@gmail.com>
2022-05-20 11:48:48 +0900

    Update types.ts

--- src/tasks/syncs/portalCheck/types.ts
+++ src/tasks/syncs/portalCheck/types.ts
@@ -15,29 +15,27 @@ export interface IProperty {
   物件番号?: string,
   物件種別?: PropertyType,
   物件名: string,
-  価格生値: string,
-  価格: number,
+  販売価格: string,
+  比較用価格: number,
   所在地: string,
   リンク: string,
   取得した日時?: string
 }
 
 export interface IHouse extends IProperty {
-  土地面積生値: string,
-  土地面積: number,
-  /** 建物面積 */
-  建物面積?: string,
+  土地面積: string,
+  比較用土地面積: number,
 }
 
 export interface ILot extends IProperty {
-  土地面積生値: string,
-  土地面積: number,
+  土地面積: string,
+  比較用土地面積: number,
 }
 
 export interface IMansion extends IProperty {
   間取り: string,
-  専有面積生値: string,
-  面積: number,
+  専有面積: string,
+  比較用専有面積: number,
 }
 
 
