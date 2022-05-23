8f7e49a872e9c8e615a9212373884514f44d190f
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-05-23 12:48:51 +0900

    SUUMO 企業情報取得処理(未完成)

--- src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/getCompanyInfo.test.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/getCompanyInfo.test.ts
@@ -2,16 +2,32 @@ import {openMockBrowserPage} from '../../../../common/browser';
 import {browserTimeOut} from '../../../../common/browser/config';
 import {IProperty} from '../../types';
 import {getCompanyInfo} from './getCompanyInfo';
-import {scrapeDtLot} from './scrapeDtLot';
 
 /* https://suumo.jp/jj/bukken/ichiran/JJ012FC002/?ar=050&bs=030&ekTjCd=&ekTjNm=&kb=1&kj=9&km=1&kt=9999999&sc=21202&ta=21&tb=0&tj=0&tt=9999999&bknlistmodeflg=2&pc=30 */
+// こちらなし /* https://suumo.jp/chukomansion/__JJ_JJ010FJ100_arz1050z2bsz1011z2ncz198324093.html */
 
-const url = 'https://suumo.jp/ms/chuko/aichi/sc_nagoyashichikusa/nc_97454649/';
-const data:IProperty = {
-
+// 中古マンション用
+const data: IProperty = {
+  物件種別: '中古マンション',
+  物件名: 'ライオンズマンション東山',
+  物件番号: '97454649',
+  所在地: '愛知県名古屋市千種区東山元町２',
+  リンク: 'https://suumo.jp/ms/chuko/aichi/sc_nagoyashichikusa/nc_97454649/',
+  販売価格: '1030万円',
+  比較用価格: 1030,
 };
 
-test(('getCompanyInfo'), async ()=>{
+/* const data: IProperty = {
+  物件種別: '中古戸建',
+  物件名: 'Asobi+　千種区竹越一丁目',
+  物件番号: '198271927',
+  所在地: '愛知県名古屋市千種区竹越１',
+  リンク: 'https://suumo.jp/chukoikkodate/__JJ_JJ010FJ100_arz1050z2bsz1021z2ncz198271927.html',
+  販売価格: '2980万円',
+  比較用価格: 2980,
+}; */
+
+test(('getCompanyInfo'), async () => {
   const page = await openMockBrowserPage();
   const result = await getCompanyInfo(page, data);
 
--- src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/getCompanyInfo.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/getCompanyInfo.ts
@@ -1,6 +1,6 @@
-// こちらなし /* https://suumo.jp/chukomansion/__JJ_JJ010FJ100_arz1050z2bsz1011z2ncz198324093.html */
-
+import {link} from 'fs';
 import {Page} from 'puppeteer';
+import {extractTel} from '../../../../../utils';
 import {IProperty} from '../../types';
 
 export const getCompanyInfo = async (
@@ -13,23 +13,50 @@ export const getCompanyInfo = async (
     page.waitForNavigation(),
   ]);
 
-  await page.evaluate((el) => {
-    // マンションのみ、「所在階/構造・階建」を取得し、物件名に追加する
-    if (data.物件種別 === '中古マンション') {
-      data.物件名 = data.物件名 + ' ' + $(el).find('th:contains(所在階) ~ td')
-        .eq(0).text().trim().split('/', 1)[0];
-    }
+  console.log('処理確認::1', data.物件種別);
+  const datas = await page.evaluate((el) => {
+    // マンションのみ、「所在階」を取得し、物件名に追加する
+    const floors = $(el).find('th:contains(所在階) ~ td')
+      .eq(0).text().trim().split('/', 1)[0];
 
     // 「こちら」のリンクの有無を確認する
+    const link = $(el).find('.heikiToiawaseWindow ~ input').eq(0).val();
 
-    // 掲載企業名を取得する
+    if (!link) {
 
-    // 掲載企業の連絡先を取得する
-    /*   const companyDates = {};
+      // 掲載企業名を取得する
 
-      return companyDates;
-    }; */
+      // 掲載企業の連絡先を取得する
+      /*   const companyDates = {};
+
+        return companyDates;
+      }; */
+    }
+    return {
+      階数: floors,
+      link: link,
+    };
   });
-  return '仮';
+  if (datas.link) {
+    await Promise.all([
+      page.goto(datas.link, {waitUntil: 'networkidle2'}),
+      page.waitForNavigation(),
+    ]);
+
+    const datas2 = await page.evaluate((el) => {
+      const kigyoumei = $(el).find('.bkdt-shop-name').children('em').text();
+      const tel = $(el).find('.bkdt-shop-name ~ ul')
+        .children('li').children('em').text();
+
+      return {
+        kigyoumei: kigyoumei,
+        tel: tel,
+      };
+    });
+
+    console.log('tel::', extractTel(datas2.tel));
+  }
+
+  return datas;
 };
 
