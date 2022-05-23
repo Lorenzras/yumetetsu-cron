a6fa9d55f4c68184b292a8e930fd9754815acd1a
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-05-23 11:13:35 +0900

    update

--- src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/getCompanyInfo.test.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/getCompanyInfo.test.ts
@@ -1,15 +1,19 @@
 import {openMockBrowserPage} from '../../../../common/browser';
 import {browserTimeOut} from '../../../../common/browser/config';
+import {IProperty} from '../../types';
 import {getCompanyInfo} from './getCompanyInfo';
 import {scrapeDtLot} from './scrapeDtLot';
 
 /* https://suumo.jp/jj/bukken/ichiran/JJ012FC002/?ar=050&bs=030&ekTjCd=&ekTjNm=&kb=1&kj=9&km=1&kt=9999999&sc=21202&ta=21&tb=0&tj=0&tt=9999999&bknlistmodeflg=2&pc=30 */
 
 const url = 'https://suumo.jp/ms/chuko/aichi/sc_nagoyashichikusa/nc_97454649/';
+const data:IProperty = {
+
+};
 
 test(('getCompanyInfo'), async ()=>{
   const page = await openMockBrowserPage();
-  const result = await getCompanyInfo(page, url);
+  const result = await getCompanyInfo(page, data);
 
   expect(result).toMatchSnapshot();
   page.browser().disconnect();
--- src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/getCompanyInfo.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/getCompanyInfo.ts
@@ -1,21 +1,35 @@
 // こちらなし /* https://suumo.jp/chukomansion/__JJ_JJ010FJ100_arz1050z2bsz1011z2ncz198324093.html */
 
 import {Page} from 'puppeteer';
+import {IProperty} from '../../types';
 
-export const getCompanyInfo = async (page:Page, url:string) => {
+export const getCompanyInfo = async (
+  page: Page,
+  data: IProperty,
+) => {
   // 物件詳細ページを表示する
   await Promise.all([
-    page.goto(url, {waitUntil: 'networkidle2'}),
+    page.goto(data.リンク, {waitUntil: 'networkidle2'}),
     page.waitForNavigation(),
   ]);
 
-  // 「こちら」のリンクの有無を確認する
+  await page.evaluate((el) => {
+    // マンションのみ、「所在階/構造・階建」を取得し、物件名に追加する
+    if (data.物件種別 === '中古マンション') {
+      data.物件名 = data.物件名 + ' ' + $(el).find('th:contains(所在階) ~ td')
+        .eq(0).text().trim().split('/', 1)[0];
+    }
 
-  // 掲載企業名を取得する
+    // 「こちら」のリンクの有無を確認する
 
-  // 掲載企業の連絡先を取得する
+    // 掲載企業名を取得する
 
-  const companyDates = {};
+    // 掲載企業の連絡先を取得する
+    /*   const companyDates = {};
 
-  return companyDates;
+      return companyDates;
+    }; */
+  });
+  return '仮';
 };
+
