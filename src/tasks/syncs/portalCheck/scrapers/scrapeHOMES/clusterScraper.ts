bf3134109c30eb0eddadc90e5040e34e37b75957
Lorenz Ras <lenzras@gmail.com>
2022-05-24 11:38:49 +0900

    added uploader

--- src/tasks/syncs/portalCheck/scrapers/scrapeHOMES/clusterScraper.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeHOMES/clusterScraper.ts
@@ -4,11 +4,11 @@ import {getExtraPuppeteer} from '../../../../common/browser/openBrowser';
 import {browserTimeOut} from '../../../../common/browser/config';
 import {logger} from '../../../../../utils/logger';
 import {Cluster} from 'puppeteer-cluster';
-
-// import puppeteer from 'puppeteer-extra';
-// import stealthPlugin from 'puppeteer-extra-plugin-stealth';
 import {clusterTask} from './clusterTask';
 import {byAction} from './clusterQueues/byAction';
+import chokidar from 'chokidar';
+import {dlPortalCheck} from '../../config';
+import {uploadTask} from '../../clusterTasks/uploadTask';
 
 
 export interface IClusterTaskData extends IPropertyAction {
@@ -16,22 +16,37 @@ export interface IClusterTaskData extends IPropertyAction {
   cities: string[]
 }
 
+const initCluster = () => Cluster.launch({
+  puppeteer: getExtraPuppeteer(),
+  concurrency: Cluster.CONCURRENCY_CONTEXT,
+  maxConcurrency: 5,
+  retryLimit: 2,
+  retryDelay: 2000,
+  puppeteerOptions: {
+    headless: false,
+  },
+  timeout: browserTimeOut,
+});
+
+const initFileWatcher = () => {
+  return chokidar.watch(dlPortalCheck, {
+    ignored: /(^|[/\\])\../, // ignore dotfiles
+    ignoreInitial: true,
+    depth: 0,
+  });
+};
+
 
 /**
  * Concurrent processing of actions defined above.
  * Refer to scrapeHOMES for synchronous processing.
  */
 export const clusterScraper = async () => {
-  const cluster: Cluster<IClusterTaskData> = await Cluster.launch({
-    puppeteer: getExtraPuppeteer(),
-    concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency: 5,
-    retryLimit: 2,
-    retryDelay: 2000,
-    puppeteerOptions: {
-      headless: false,
-    },
-    timeout: browserTimeOut,
+  const cluster : Cluster<IClusterTaskData> = await initCluster();
+  const watcher = initFileWatcher();
+
+  watcher.on('add', (path)=>{
+    cluster.execute(({page})=> uploadTask(page, path));
   });
 
   logger.info(`Starting cluster.`);
