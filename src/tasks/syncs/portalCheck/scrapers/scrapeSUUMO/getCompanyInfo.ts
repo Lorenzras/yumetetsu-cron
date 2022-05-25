5661b612bfe15b2efd75074944295929d606a8a7
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-05-25 10:49:42 +0900

    Update getCompanyInfo.ts

--- src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/getCompanyInfo.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/getCompanyInfo.ts
@@ -1,4 +1,3 @@
-import {link} from 'fs';
 import {Page} from 'puppeteer';
 import {extractTel} from '../../../../../utils';
 import {IProperty} from '../../types';
@@ -22,41 +21,51 @@ export const getCompanyInfo = async (
     // 「こちら」のリンクの有無を確認する
     const link = $(el).find('.heikiToiawaseWindow ~ input').eq(0).val();
 
+    // 「こちら」のリンクがない場合
+    let kigyoumei = '';
+    let tel = '';
     if (!link) {
-
       // 掲載企業名を取得する
-
+      kigyoumei = '';
       // 掲載企業の連絡先を取得する
-      /*   const companyDates = {};
+      tel = '';
+      /* const companyDates = {};
 
         return companyDates;
       }; */
     }
+
     return {
       階数: floors,
       link: link,
+      掲載企業: kigyoumei,
+      掲載企業TEL: tel,
     };
   });
+
+  console.log('return check 1::', datas);
+
+  // 「こちら」のリンクがある場合
   if (datas.link) {
+    // リンク先にジャンプする
     await Promise.all([
       page.goto(datas.link, {waitUntil: 'networkidle2'}),
       page.waitForNavigation(),
     ]);
 
-    const datas2 = await page.evaluate((el) => {
+    await page.evaluate((el) => {
       const kigyoumei = $(el).find('.bkdt-shop-name').children('em').text();
       const tel = $(el).find('.bkdt-shop-name ~ ul')
         .children('li').children('em').text();
 
-      return {
+      return {...datas,
         kigyoumei: kigyoumei,
-        tel: tel,
+        tel: extractTel(tel),
       };
     });
 
-    console.log('tel::', extractTel(datas2.tel));
+    console.log('datas::', datas);
   }
 
   return datas;
 };
-
