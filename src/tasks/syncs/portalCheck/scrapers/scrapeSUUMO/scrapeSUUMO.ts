bc999560ebf80d9c877e5df5c2ad17b3e2708f02
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-05-25 14:44:29 +0900

    Update scrapeSUUMO.ts

--- src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/scrapeSUUMO.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/scrapeSUUMO.ts
@@ -6,8 +6,10 @@ import {scrapeDtApartment} from './scrapeDtApartment';
 import {scrapeDtHouse} from './scrapeDtHouse';
 import {scrapeDtLot} from './scrapeDtLot';
 import {scrapeLoop} from './scrapeLoop';
-import {searchClick} from './searchClick';
 
+/**
+ * @deprecated
+ */
 const propertyActions: PropertyActions = [
   {
     type: '中古戸建',
@@ -27,6 +29,7 @@ const propertyActions: PropertyActions = [
 ];
 
 /**
+ *  @deprecated
  * スーモサイトのスクレイピング設定
  * 物件種別ごとに、citylistから抽出した県の検索ページを開き、
  * 市毎の検索処理に情報を受け渡す
@@ -37,17 +40,14 @@ export const scrapeSUUMO = async (page: Page) => {
   for (const actions of propertyActions) {
     // citylistのkey(県)毎に処理を繰り返す
     for (const [pref, cityInfo] of Object.entries(location)) {
-      const cities = Object.keys(cityInfo); // 県ごとに市の配列を作成する
-
-      const targetPref = pref === '愛知' ?
+      const targetPref = pref === '愛知県' ?
         actions.url + 'aichi/city/' : actions.url + 'gifu/city/';
 
       // 検索サイトへ移動する
       // メモ：networkidle2=最後に通信が発生してから500ms待つ
       await page.goto(targetPref, {waitUntil: 'networkidle2'});
 
-      await prepareForm(page, cities); // 検索条件を選択する
-      await searchClick(page); // 検索ボタンを押す
+      await prepareForm(page, pref, actions.type); // 検索条件を選択する
       const result = await scrapeLoop(page, actions.handleScraper); // スクレイピング処理
     }
   }
