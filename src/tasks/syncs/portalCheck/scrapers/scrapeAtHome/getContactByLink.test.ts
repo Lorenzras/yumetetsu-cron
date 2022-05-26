cf2c3e41e689a76568f377e48dc20046f82ef8a5
Lorenz Ras <lenzras@gmail.com>
2022-05-26 10:16:42 +0900

    added atHome fix contact page

--- src/tasks/syncs/portalCheck/scrapers/scrapeAtHome/getContactByLink.test.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeAtHome/getContactByLink.test.ts
@@ -10,15 +10,21 @@ test('getContact', async ()=>{
     'https://www.athome.co.jp/kodate/1022733165',
     'https://www.athome.co.jp/tochi/1040134670/',
     'https://www.athome.co.jp/mansion/1066314746/',
+    'https://www.athome.co.jp/kodate/3915789702/',
+    'https://www.athome.co.jp/kodate/1031689167/',
+    'https://www.athome.co.jp/kodate/3915824302/',
   ];
 
   const result = [];
   for (const url of urls) {
-    result.push( await getContactByLink(page, url));
+    console.log('Processing url ', url);
+    const pageRes = await getContactByLink(page, url);
+    console.log(pageRes);
+    result.push( {...pageRes, url});
   }
 
-  expect(result).toMatchSnapshot();
 
+  expect(result).toMatchSnapshot();
   page.browser().disconnect();
 }, browserTimeOut);
 
