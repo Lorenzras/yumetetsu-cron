f2dbc44583507858ffa271a10e770764fad8ec3c
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-05-30 14:17:19 +0900

    Yahoo prepareForm完了

--- src/tasks/syncs/portalCheck/scrapers/scrapeYahoo/prepareForm.test.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeYahoo/prepareForm.test.ts
@@ -1,23 +1,30 @@
 import {openMockBrowserPage} from '../../../../common/browser';
 import {browserTimeOut} from '../../../../common/browser/config';
-import {prepareForm, searchYahoo} from './prepareForm';
+import {prepareForm} from './prepareForm';
 
 /* https://suumo.jp/ms/chuko/aichi/city/ */
 
-describe('prepareForm', () => {
-  test(('prepareForm'), async () => {
-    const page = await openMockBrowserPage();
-    await prepareForm(page, '愛知県', '中古マンション');
+// describe('prepareForm', () => {
+/* test comand:: jest scrapeYahoo/prepareForm -t check -u */
+test(('test1'), async () => {
+  const page = await openMockBrowserPage();
+  await prepareForm(page, '愛知県', '中古マンション', 1);
 
-    page.browser().disconnect();
-  }, browserTimeOut);
+  page.browser().disconnect();
+}, browserTimeOut);
 
-
-  /* test comand:: jest scrapeYahoo/prepareForm -t click -u */
-  test(('click'), async () => {
+/* test comand:: jest scrapeYahoo/prepareForm -t click -u */
+/* test(('click'), async () => {
     const page = await openMockBrowserPage();
     await searchYahoo(page);
 
     page.browser().disconnect();
   }, browserTimeOut);
-});
+
+  test(('check'), async () => {
+    const page = await openMockBrowserPage();
+    await checkCities(page);
+
+    page.browser().disconnect();
+  }, browserTimeOut); */
+// });
