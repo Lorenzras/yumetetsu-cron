370c220496912e319a7642f45cccc987a5bc951f
Lorenz Ras <lenzras@gmail.com>
2022-05-31 11:44:20 +0900

    it will now wait for the search button to appear before clicking it.

--- src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/prepareForm.test.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeSUUMO/prepareForm.test.ts
@@ -1,12 +1,15 @@
-import {openMockBrowserPage} from '../../../../common/browser';
+import {openBrowserPage, openMockBrowserPage} from '../../../../common/browser';
 import {browserTimeOut} from '../../../../common/browser/config';
 import {prepareForm} from './prepareForm';
 
 /* https://suumo.jp/ms/chuko/aichi/city/ */
 
 test(('prepareForm'), async ()=>{
-  const page = await openMockBrowserPage();
-  await prepareForm(page, '愛知県', '土地');
+  const page = await openBrowserPage();
+  // await page.goto('https://suumo.jp/ms/chuko/aichi/city');
+  const result = await prepareForm(page, '愛知県', '中古戸建');
 
-  page.browser().disconnect();
+  // page.browser().disconnect();
+  await page.close();
+  expect(result).toBeTruthy();
 }, browserTimeOut);
