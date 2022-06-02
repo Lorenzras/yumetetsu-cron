0252a4088bd981080525ab058f3f4948093838ae
Lorenz Ras <lenzras@gmail.com>
2022-06-02 11:52:00 +0900

    Update scrapeDtLot.ts

--- src/tasks/syncs/portalCheck/scrapers/scrapeYahoo/scrapeDtLot.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeYahoo/scrapeDtLot.ts
@@ -4,12 +4,11 @@ import {logger} from '../../../../../utils';
 import {ILot} from '../../types';
 import {webScraper} from './../helpers/webScraper';
 import {getDataEndpoint} from './helpers/getDataEndpoint';
-
 import {load} from 'cheerio';
+import {handleNextPage} from './helpers/handleNextPage';
 
 export const scrapeDtLotPage = async (page: Page) => {
   const htmlBody = await page.content();
-
 
   const $ = load(htmlBody);
   const els = $('.ListBukken__list__item:not(._devListBoxWrap)').toArray();
@@ -18,10 +17,10 @@ export const scrapeDtLotPage = async (page: Page) => {
     const link = $(el).find('button').attr('data-dtlurl') ?? '';
     const address = $(el).find(':contains(所在地)').next('dd').text().trim();
     const price = $(el).find(':contains(価格)').next('dd').text().trim();
-
+    const lotArea = $(el).find(':contains(土地面積)').next('dd').text().trim();
     return {
       リンク: link,
-      土地面積: 'lotArea',
+      土地面積: lotArea,
       所在地: address,
       比較用価格: 0,
       比較用土地面積: 0,
@@ -33,18 +32,6 @@ export const scrapeDtLotPage = async (page: Page) => {
   );
 };
 
-export const handleNextPage = async (page: Page) => {
-  const hasNextPage = await page.$eval('[data-has_next_page]', (el) => {
-    return JSON.parse((el as HTMLElement).dataset.has_next_page ?? 'false');
-  });
-
-  if (hasNextPage) {
-    const rawPage = page.url().split('&')?.at(-1) ?? '';
-    const nextPageNum = +(rawPage?.split('=').at(-1) ?? 0) + 1;
-    await page.goto(page.url().replace(rawPage, `page=${nextPageNum}`));
-  }
-  return hasNextPage;
-};
 
 export const scrapeDtLot = async (
   page: Page,
