0fcdc8a9796b0768f8c522f69f3c341cea7f5eaf
Lorenz Ras <lenzras@gmail.com>
2022-06-03 14:08:39 +0900

    added cron for portalCheck

--- src/tasks/syncs/portalCheck/scrapers/scrapeHOMES/scrapeDtLot.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeHOMES/scrapeDtLot.ts
@@ -18,23 +18,22 @@ export const scrapeDtLotPage = async (page: Page) => {
         const propUrl: string = $(el).find('.prg-bukkenNameAnchor')
           .prop('href');
 
-        let areas: string[] = [];
+        let areas = '';
         /* Some properties have their details
         listed at the bottom of the card */
         const unitSummary = $(el).find('.unitSummary');
 
         if (unitSummary.length) {
-          areas[0] = unitSummary.find('tbody tr:first td.space:first')
-            .text().replaceAll('m²', '');
+          areas = unitSummary.find('tbody tr:first td.space:first')
+            .text();
         } else {
           areas = $(el)
             .find('th:contains(土地面積)')
             .siblings('td:first').text()
-            .trim().replaceAll('m²', '').split('〜', 2);
+            .trim();
         }
 
 
-        const [rawLotArea] = areas;
         return {
           物件番号: 'homes-' + propUrl.split('/')
             .reduce((accu, curr) => curr ? curr : accu),
@@ -43,7 +42,7 @@ export const scrapeDtLotPage = async (page: Page) => {
           比較用価格: 0,
           所在地: address,
           比較用土地面積: 0,
-          土地面積: rawLotArea,
+          土地面積: areas,
           リンク: propUrl,
         };
       });
@@ -51,14 +50,7 @@ export const scrapeDtLotPage = async (page: Page) => {
 
     logger.info(`Scraped lot page ${data.length}`);
 
-    return data
-      .map(((item)=>{
-        return ({
-          ...item,
-          比較用価格: extractPrice(item.販売価格.split('円')[0]),
-          比較用土地面積: extractNumber(item.土地面積),
-        });
-      }));
+    return data;
   } catch (err: any) {
     logger.error(`HOMES scrapeDtLotPage ${err.message}`);
     throw new Error(err);
