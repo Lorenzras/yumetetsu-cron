aa958505f406520636c5b2cd147fc08c3f145739
Lorenz Ras <lenzras@gmail.com>
2022-06-03 12:18:25 +0900

    Update handleDonetCompare.ts

--- src/tasks/syncs/portalCheck/clusterTasks/handleDonetCompare.ts
+++ src/tasks/syncs/portalCheck/clusterTasks/handleDonetCompare.ts
@@ -68,36 +68,6 @@ export const handleDonetCompare = async (
         };
       }) as IProperty;
     }));
-    /*  return await cluster.execute(async ({page, worker}) => {
-      let pIdx = 0;
-      const newProps: IProperty[] = [];
-      for (const prop of props) {
-        const doNetComparedResults = await searchDoProperty(
-          {page, inputData: prop},
-        ) ?? {
-          DO管理有無: '処理エラー',
-        } as IProperty;
-        const firstComparedResult = doNetComparedResults[0];
-
-        logger.info(`Compared ${
-          [
-            (pIdx + 1) + ' IProperty ',
-            propsLength + ' IProperty[] ',
-            (psIdx + 1) + ' chunk ',
-            chunkLength + ' chunk[] ',
-          ].join(' / ')
-        }  with total length ${dtArrLength} `);
-
-        newProps.push({
-          ...prop,
-          ...firstComparedResult,
-        });
-
-        pIdx++;
-      }
-
-      return newProps;
-    }) as IProperty[]; */
   }));
 
 
