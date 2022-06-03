d31b15a1c2eefc953ff2e4e618a3cfd307def937
Lorenz Ras <lenzras@gmail.com>
2022-06-03 13:22:30 +0900

    casing

--- src/tasks/syncs/portalCheck/scrapers/scrapeHOMES/index.ts
+++ src/tasks/syncs/portalCheck/scrapers/scrapeHOMES/index.ts
@@ -2,7 +2,7 @@ export * from './actionsHOMES';
 export * from './getContact';
 export * from './handleContactScraper';
 export * from './handleNextPage';
-export * from './handlePrepareForm';
+export * from './handlePrepareFoorm';
 export * from './perCity';
 export * from './scrapeDtHouse';
 export * from './scrapeDtLot';
