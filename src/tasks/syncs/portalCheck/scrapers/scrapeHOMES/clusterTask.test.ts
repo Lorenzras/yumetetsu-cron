8f7c9b938e640391b35f623c87a9efda2ef2ee80
Lorenz Ras <lenzras@gmail.com>
2022-06-03 13:17:49 +0900

    Clean up. For deployment

--- src/tasks/syncs/portalCheck/scrapers/scrapeHOMES/clusterTask.test.ts
+++
@@ -1,7 +0,0 @@
-/* import {browserTimeOut} from '../../../../common/browser/config';
-import {clusterScraper} from './clusterScraper';
-test('advancedScraper', async ()=>{
-  const result = await clusterScraper();
-  expect(result).toMatchSnapshot();
-}, browserTimeOut);
- */
