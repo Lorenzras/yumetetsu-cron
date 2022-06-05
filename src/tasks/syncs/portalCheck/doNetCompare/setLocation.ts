65a35ca0a2ec04d8447c56f15894524fab01483f
Lorenz Ras <lenzras@gmail.com>
2022-06-05 10:02:04 +0900

    Update setLocation.ts

--- src/tasks/syncs/portalCheck/doNetCompare/setLocation.ts
+++ src/tasks/syncs/portalCheck/doNetCompare/setLocation.ts
@@ -16,29 +16,29 @@ export const setLocation = async (
   }) => {
   const {pref, city, town} = data;
   await retry(async ()=>{
-    logger.info(`Setting location. ${pref} ${city} ${town}`);
+    // Test reset in case of retrying
     await page.click('#modal_clear_button').catch(()=>null);
+
+    logger.info(`Setting location. ${pref} ${city} ${town}`);
 
     /* Open modal */
     await page.waitForSelector('#select_button_city1');
     await page.click('#select_button_city1');
 
-    // await selectByText(page, '#select_pref_id', '愛媛県' );
-    // await selectByText(page, '#select_pref_id', pref );
-
-    // await page.click('#modal_city_name_autocomplete_list_button');
     await Promise.all([
       selectByText(page, '#select_pref_id', pref ),
       page.waitForResponse((resp) => {
-        return resp.url().includes('https://manage.do-network.com/m_city/list');
-      })]);
+        return resp.url().includes('https://manage.do-network.com/m_city/list') &&
+        resp.status() === 200;
+      }, {timeout: 10000}).catch(),
+    ]);
 
     await page.click('#modal_city_name_autocomplete', {clickCount: 3});
     await page.type('#modal_city_name_autocomplete', city);
 
     if (town) {
-      // On slow or laggy computers, donet's town API takes a very long time to respond or populate the town field
-      // Addressing it with the following lines
+      // On slow or laggy computers, donet's town API takes a very long time to respond so
+      // I addressing it with the following lines
 
       await Promise.all([
         page.$eval(
@@ -46,18 +46,21 @@ export const setLocation = async (
             (e as HTMLInputElement).blur();
           }),
         page.waitForResponse((resp) => {
-          return resp.url().includes('https://manage.do-network.com/m_town/list');
-        }),
+          return resp.url().includes('https://manage.do-network.com/m_town/list') &&
+           resp.status() === 200;
+        }).catch(),
       ]);
 
+      // Click/Focus to trigger populate when trigger blur event failed due to network lag.
+      await page.click('#modal_town_name_autocomplete');
 
       await page.waitForSelector(
         '#modal_town_name_autocomplete:not(:disabled)',
         {timeout: 15000},
       )
         .then(async ()=>{
-          // There are times when blur doesn't fire to populate autocomplete.
-          // So 3x click the field.
+          // Setting the value directly does not reliably trigger autocomplete so
+          // 3x click the field to simulate a person overwriting town field.
           await page.click('#modal_town_name_autocomplete', {clickCount: 3});
           return page.type('#modal_town_name_autocomplete', town);
         })
