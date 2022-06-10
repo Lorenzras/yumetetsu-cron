3cab446316f267f200b4cf6e9da5830b3673bd32
Lorenz Ras <lenzras@gmail.com>
2022-06-11 08:13:18 +0900

    Update saveMeta.test.ts

--- src/tasks/syncs/portalCheck/helpers/saveMeta.test.ts
+++ src/tasks/syncs/portalCheck/helpers/saveMeta.test.ts
@@ -1,10 +1,11 @@
+/* eslint-disable max-len */
 import {IProperty} from './../types';
 import {browserTimeOut} from './../../../common/browser/config';
 import {saveMeta} from './saveMeta';
-//import before from './test/199-20220605-193449-A84sd--doComparedDt-5470.json';
-//import after from './test/199-20220605-195446-zDzyj--finalResults-2451.json';
-import before from "../../../../../downloads/json/portalCheck/199-20220608-225751-SCE1a--doComparedDt-5245.json"
-import after from "../../../../../downloads/json/portalCheck/199-20220608-235456-N5T8V--finalResults-2426.json"
+// import before from './test/199-20220605-193449-A84sd--doComparedDt-5470.json';
+// import after from './test/199-20220605-195446-zDzyj--finalResults-2451.json';
+import before from '../../../../../downloads/json/portalCheck/199-20220611-054833-7LY7J--doComparedDt-5388.json';
+import after from '../../../../../downloads/json/portalCheck/199-20220611-062546-MkkXJ--finalResults-2432.json';
 test('meta', ()=>{
   saveMeta(before as IProperty[], after as IProperty[]);
 }, browserTimeOut);
