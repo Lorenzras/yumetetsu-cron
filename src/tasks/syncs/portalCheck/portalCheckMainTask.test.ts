34677f5d4d0be47e7a42b491c940a4d2c24e9851
rpa03 <95675193+rpa03@users.noreply.github.com>
2022-06-10 11:58:50 +0900

    Refactored

--- src/tasks/syncs/portalCheck/portalCheckMainTask.test.ts
+++ src/tasks/syncs/portalCheck/portalCheckMainTask.test.ts
@@ -1,42 +1,42 @@
 /* eslint-disable max-len */
-import {scraperTask} from './clusterTasks/scraperTask';
-import {browserTimeOut} from '../../common/browser/config';
-import {initCluster, portalCheckMainTask} from './portalCheckMainTask';
+import { scraperTask } from './clusterTasks/scraperTask';
+import { browserTimeOut } from '../../common/browser/config';
+import { initCluster, portalCheckMainTask } from './portalCheckMainTask';
 import _ from 'lodash';
-import {Cluster} from 'puppeteer-cluster';
-import {Page} from 'puppeteer';
+import { Cluster } from 'puppeteer-cluster';
+import { Page } from 'puppeteer';
 import fs from 'fs';
 import path from 'path';
 
 
-import {saveToExcel} from './excelTask/saveToExcel';
-import {IProperty} from './types';
-import {handleGetCompanyDetails} from './clusterTasks/handleGetCompanyDetails';
-import {actionsHOMES} from './scrapers/scrapeHOMES';
-import {actionsAtHome} from './scrapers/scrapeAtHome/actionsAtHome';
+import { saveToExcel } from './excelTask/saveToExcel';
+import { IProperty } from './types';
+import { handleGetCompanyDetails } from './clusterTasks/handleGetCompanyDetails';
+import { actionsHOMES } from './scrapers/scrapeHOMES';
+import { actionsAtHome } from './scrapers/scrapeAtHome/actionsAtHome';
 import {
   suumoActions as actionsSUUMO,
 } from './scrapers/scrapeSUUMO/suumoActions';
 import {
   yahooActions as actionsYahoo,
 } from './scrapers/scrapeYahoo/yahooActions';
-import {resultJSONPath} from './config';
-import {saveMeta} from './helpers/saveMeta';
-import {logger, sleep} from '../../../utils';
+import { resultJSONPath } from './config';
+import { saveMeta } from './helpers/saveMeta';
+import { logger, sleep } from '../../../utils';
 
 const getJSONData = (fName: string) => {
   const res = fs.readFileSync(path.join(resultJSONPath, fName), 'utf8');
   return JSON.parse(res);
 };
 
-describe('portalCheckMainProcess', ()=>{
-  it('mainprocess', async ()=>{
+describe('portalCheckMainProcess', () => {
+  it('mainprocess', async () => {
     await portalCheckMainTask();
     expect(true);
   }, browserTimeOut);
 
-  it('lite', async ()=>{
-    const cluster: Cluster<{page: Page}> = await initCluster();
+  it('lite', async () => {
+    const cluster: Cluster<{ page: Page }> = await initCluster();
 
     const actions = [
       // ...actionsHOMES(),
@@ -54,31 +54,35 @@ describe('portalCheckMainProcess', ()=>{
     await cluster.close();
   }, browserTimeOut);
 
-  it('contacts', async ()=>{
+  it('contacts', async () => {
     // 失敗したものを取得させる
     const jsonFName = '199-20220610-074022-QkBts--doComparedDt-5198.json';
-    const cluster: Cluster<{page: Page}> = await initCluster();
+    const cluster: Cluster<{ page: Page }> = await initCluster();
     const data: IProperty[] = getJSONData(jsonFName);
     const filteredData = data
-      .filter((dt)=>{
+      .filter((dt) => {
         return (
           !dt.DO管理有無 ||
-        dt.DO管理有無 === '無' ||
-        (dt.DO管理有無 === '有' && +(dt.DO価格差 ?? 0) !== 0)
+          dt.DO管理有無 === '無' ||
+          (dt.DO管理有無 === '有' && +(dt.DO価格差 ?? 0) !== 0)
         );
       });
 
     const filterDataLength = filteredData.length;
     const newData = await Promise.all(_.shuffle(filteredData)
       .map(async (item, idx) => {
-        if (!item.掲載企業TEL?.trim()) {
-          return await cluster.execute(async ({page, worker}) => {
-            logger.info(`Worker ${worker.id} at ${idx} of ${filterDataLength} is fetching contact from ${item.リンク}`);
-            return await handleGetCompanyDetails(page, item);
-          }) as IProperty;
-        }
 
-        return item;
+        
+
+        return await cluster.execute(async ({ page, worker }) => {
+          logger.info(`Worker ${worker.id} at ${idx} of ${filterDataLength} is fetching contact from ${item.リンク}`);
+          const res = await handleGetCompanyDetails(page, item)
+          logger.info(`Worker ${worker.id} at ${idx} of ${filterDataLength} is DONE fetching contact from ${item.リンク}`)
+          return res;
+          
+        }) as IProperty;
+
+        
       }));
 
     await saveToExcel(newData);
