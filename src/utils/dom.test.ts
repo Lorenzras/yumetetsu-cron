cdd3818a5a5d4d1ac1c0ea3bd65efd5ce3c5b59d
Lorenz Ras <lenzras@gmail.com>
2022-06-11 15:42:45 +0900

    added select test

--- src/utils/dom.test.ts
+++ src/utils/dom.test.ts
@@ -1,20 +1,43 @@
+import {launchBrowser} from './../tasks/common/browser/openBrowser';
+import puppeteer from 'puppeteer-extra';
 import {openMockBrowserPage} from '../tasks/common/browser';
 import {browserTimeOut} from '../tasks/common/browser/config';
 import {getTextByXPath} from './dom';
 
-test('dom', async ()=>{
-  const page = await openMockBrowserPage();
+describe('dom', ()=>{
+  test('getTextByXPath', async ()=>{
+    const page = await openMockBrowserPage();
 
-  const testEl = (await page.$$('.ListBukken__list'))[1];
-  await page.evaluate((el) => el.style.border = '5px solid red', testEl);
+    const testEl = (await page.$$('.ListBukken__list'))[1];
+    await page.evaluate((el) => el.style.border = '5px solid red', testEl);
 
-  if (!testEl) return;
+    if (!testEl) return;
 
-  const text = await getTextByXPath(
-    page, './/dt[contains(text(), "価格")]/following-sibling::dd', testEl,
-  );
+    const text = await getTextByXPath(
+      page, './/dt[contains(text(), "価格")]/following-sibling::dd', testEl,
+    );
 
-  console.log(text);
+    console.log(text);
 
-  page.browser().disconnect();
-}, browserTimeOut);
+    page.browser().disconnect();
+  }, browserTimeOut);
+  test('select', async ()=>{
+    const page = await puppeteer.launch({
+      headless: false,
+    })
+      .then((b) => b.pages())
+      .then(([p] )=> p);
+
+    await page.goto('https://www.w3docs.com/learn-html/html-select-tag.html');
+
+
+    await page.waitForSelector('select[aria-label="Books"]')
+      .then((s) => s?.select('js'));
+
+
+    await page.waitForTimeout(5000);
+
+    await page.browser().close();
+  }, browserTimeOut);
+});
+
