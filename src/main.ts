41d20171c20b0364610d8b5be5dd8e5501587870
Lorenz Ras <lenzras@gmail.com>
2022-06-13 10:25:09 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,9 +19,9 @@ cron.schedule('30 9 * * *', remindOnLongtermCust, cronOptions);
 /**
  * Full sync donet customers to kintone.
  *
- * At 18:00 on Sunday.
+ * At 20:00 on Sunday.
  */
-cron.schedule('0 18 * * Sun', () => syncDoNetCust(true), cronOptions);
+cron.schedule('0 20 * * Sun', () => syncDoNetCust(true), cronOptions);
 
 /**
  * Full sync donet customers to kintone.
